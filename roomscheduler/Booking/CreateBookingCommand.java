f6356720e9aad45f265a75e5ab43a8c2bd64684b
Loveneek <singhloveneek8@gmail.com>
2025-11-22 16:35:56 -0500

    Updated Create BookingCommand

--- roomscheduler/Booking/CreateBookingCommand.java
+++ roomscheduler/Booking/CreateBookingCommand.java
@@ -1,15 +1,56 @@
+// package Booking;
+
+// public class CreateBookingCommand implements BookingCommand {
+//     private Booking booking;
+
+//     public CreateBookingCommand(Booking booking) {
+//         this.booking = booking;
+//     }
+
+//     @Override
+//     public void execute() {
+//         booking.create();
+//     }
+    
+// }
 package Booking;
 
-public class CreateBookingCommand implements BookingCommand {
-    private Booking booking;
+import java.time.LocalDateTime;
 
-    public CreateBookingCommand(Booking booking) {
-        this.booking = booking;
+public class CreateBookingCommand implements BookingCommand {
+    private String bookingID;
+    private Booking.TempUserType userType;
+    private String roomID;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+
+    private Booking createdBooking; // store the new booking
+
+    public CreateBookingCommand(String bookingID,
+                                Booking.TempUserType userType,
+                                String roomID,
+                                LocalDateTime checkIn,
+                                LocalDateTime checkOut) {
+        this.bookingID = bookingID;
+        this.userType = userType;
+        this.roomID = roomID;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     @Override
     public void execute() {
-        booking.create();
+        createdBooking = new Booking(
+                bookingID,
+                userType,
+                roomID,
+                checkIn,
+                checkOut
+        );
+        createdBooking.create(); // run pricing + deposit logic
     }
-    
+
+    public Booking getCreatedBooking() {
+        return createdBooking;
+    }
 }
